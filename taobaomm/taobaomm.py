eb6c5286987ee31102cc00cf776c4595226d44cd
Archiew <1227254255@qq.com>
2017-11-02 18:12:50 +0800

    commit

--- taobaomm/taobaomm.py
+++ taobaomm/taobaomm.py
@@ -15,6 +15,28 @@ param = {'_input_charset': 'utf-8',
           'pageSize': '20'
           }
 url = 'https://mm.taobao.com/tstar/search/tstar_model.do'
+tablename = 'pages'
+conn = pymysql.connect(host='127.0.0.1', user='root', passwd='root', db='mysql', charset='utf8')
+cur = conn.cursor()
+cur.execute('USE wyq')
+try:
+    cur.execute('CREATE TABLE '+tablename+' (id BIGINT(7) NOT NULL AUTO_INCREMENT, name VARCHAR(100), city VARCHAR(20), height VARCHAR(10), weight VARCHAR(10), homepage VARCHAR(100), profile VARCHAR(100), pic VARCHAR(100), created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id))')
+except pymysql.err.InternalError as e:
+    print(e)
+
+cur.execute('ALTER DATABASE wyq CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci')
+cur.execute('ALTER TABLE '+tablename+' CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci')
+cur.execute('ALTER TABLE '+tablename+' CHANGE name name VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci')
+cur.execute('ALTER TABLE '+tablename+' CHANGE city city VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci')
+cur.execute('ALTER TABLE '+tablename+' CHANGE height height VARCHAR(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci')
+cur.execute('ALTER TABLE '+tablename+' CHANGE weight weight VARCHAR(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci')
+cur.execute('ALTER TABLE '+tablename+' CHANGE homepage homepage VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci')
+cur.execute('ALTER TABLE '+tablename+' CHANGE profile profile VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci')
+cur.execute('ALTER TABLE '+tablename+' CHANGE pic pic VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci')
+
+def store(name, city, height, weight, hompage, profile, pic):
+    cur.execute('INSERT INTO '+tablename+' (name, city, height, weight, homepage, profile, pic) VALUES (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\")', (name, city, height, weight, hompage, profile, pic))
+    cur.connection.commit()
 
 
 def getJson(page):
@@ -34,27 +56,28 @@ def parserJson(content, page):
     data = content['data']['searchDOList']
     for l in data:
         temp = {}
-        temp['id'] = l['userId']
+        temp['id'] = str(l['userId'])
         temp['name'] = l['realName']
         temp['city'] = l['city']
-        temp['height'] = l['height']
-        temp['weight'] = l['weight']
-        temp['favornum'] = l['totalFavorNum']
+        temp['height'] = str(l['height'])
+        temp['weight'] = str(l['weight'])
+        temp['favornum'] = str(l['totalFavorNum'])
         temp['profile'] = 'http:'+l['avatarUrl']
         temp['pic'] = 'http:'+l['cardUrl']
         #print(temp)
-        mmList.append(temp)
+        #mmList.append(temp)
         mkdir(temp['name'])
         print('%s正在抓取%s'%(page, temp['name']))
         getImg(temp['profile'], temp['name'], 'profile')
         getImg(temp['pic'], temp['name'], 'pic')
-        with open('./'+temp['name']+'/info.txt', 'w') as f:
-            f.write(temp['name']+'\n')
-            f.write(temp['city']+'\n')
-            f.write(temp['height']+'\n')
-            f.write(temp['weight']+'\n')
-
-    return mmList
+        if not os.path.exists('./'+temp['name']+'/info.txt'):
+            with open('./'+temp['name']+'/info.txt', 'w') as f:
+                f.write(temp['name']+'\n')
+                f.write(temp['city']+'\n')
+                f.write(temp['height']+'\n')
+                f.write(temp['weight']+'\n')
+        store(temp['name'], temp['city'], temp['height'], temp['weight'], 'https://mm.taobao.com/self/aiShow.htm?userId='+temp['id'], temp['profile'], temp['pic'])
+    #return mmList
 
 
 def mkdir(path):
@@ -77,14 +100,15 @@ def getImg(url, path, name):
     except error.URLError as e:
         print(e.reason)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     page = 1
     while True:
         content = getJson(page)
         if content == -1:
             print('抓取完毕！')
             exit()
-        mm = parserJson(content, page)
+        parserJson(content, page)
         page += 1
 
 
